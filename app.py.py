4cf84f2b23164b4892eb9e47ff12539df61c2d00
Daniel <danielgaston6@gmail.com>
2018-03-24 18:57:36 -0400

    initial

--- app.py.py
+++ app.py.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return "Hello World"
+    render_template('index.html')
 
 if __name__=="__main__":
     app.run()
