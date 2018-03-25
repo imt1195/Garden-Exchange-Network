625ff933315116753ce64b02ed5a73331bb363d1
Daniel <danielgaston6@gmail.com>
2018-03-24 21:29:10 -0400

    h

--- app.py.py
+++ app.py.py
@@ -8,6 +8,15 @@ def index():
 @app.route('/login')
 def loginPage():
 return render_template('LoginPage.html')
+@app.route('/map')
+def loginPage():
+return render_template('map.html')
+@app.route('/member')
+def memberActivities():
+return render_template('Member_Actvities.html')
+@app.route('/test')
+def testingPage():
+return render_template('TestingPage.html')
 
 
 if __name__=="__main__":
