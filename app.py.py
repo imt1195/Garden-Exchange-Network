d013281430db54e0d88e5224105769a43fdb06cc
Daniel <danielgaston6@gmail.com>
2018-03-24 21:34:20 -0400

    meep

--- app.py.py
+++ app.py.py
@@ -5,15 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/login')
 def loginPage():
 return render_template('LoginPage.html')
+
 @app.route('/map')
-def loginPage():
+def map():
 return render_template('map.html')
+
 @app.route('/member')
 def memberActivities():
 return render_template('Member_Actvities.html')
+
 @app.route('/test')
 def testingPage():
 return render_template('TestingPage.html')
