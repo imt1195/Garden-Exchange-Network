d91d190911b7a349dc68cc3e9fce7326b94727d8
Daniel <danielgaston6@gmail.com>
2018-03-24 20:01:16 -0400

    new

--- app.py.py
+++ app.py.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/login')
+def loginPage():
+return render_template('LoginPage.html')
+
 
 if __name__=="__main__":
     app.run()
